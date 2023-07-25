22170cc0eb22efebc8e1aee81ca0659b8019cc77
leonardo lerario <leonardo_lerario@hotmail.it>
2023-07-25 16:34:05 +0200

    finish announcemnts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnnouncementController;
 use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PublicController::class, 'home'])->name('home');
+Route::get('/announcement/create', [AnnouncementController::class, 'create'])->name('announcement.create');
