94daf474ad06482e6a339ce5f3ae59fa6fcc1db0
Graziano-Calella <graziano.calella@gmail.com>
2023-07-26 13:04:30 +0200

    finish user story 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
                           <p class="card-text">{{$announcement->description}}</p>
                           <p class="card-text">{{$announcement->price}}</p>
                           <a href="{{route('announcement.show',compact('announcement'))}}" class="btn btn-primary shadow">Visualizza</a>
-                          <a href="#" class="btn my-2 border-top pt-2 border-dark card-link shadow btn-success">Categoria : {{$announcement->category->name}}</a>
+                          <a href="{{route('category.show', $announcement->category)}}" class="btn my-2 border-top pt-2 border-dark card-link shadow btn-success">Categoria : {{$announcement->category->name}}</a>
                           <p class="card-footer">Pubblicato il : {{$announcement->created_at->format('d/m/Y')}}</p>
                         </div>
                       </div>
