0797875c9207170ce1595805fdd9a0836ad8fdbd
morris vattiata <vattiata97@gmail.com>
2023-07-27 13:03:13 +0200

    test morris

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,15 +4,15 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
+        <div class="row collapse navbar-collapse justify-conten-space-around" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                <li class="nav-item col-3">
                     <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="{{route('announcement.index')}}">Tutti gli Annunci</a>
                 </li>
-                    <li class="nav-item dropdown">
+                    <li class="nav-item dropdown col-3">
                         <a class="nav-link dropdown-toggle" href="#" id="categoriesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                            Categorie
                         </a>
@@ -24,10 +24,10 @@
                         </ul>
                     </li>
                 @guest
-                    <li class="nav-item">
+                    <li class="nav-item col-3">
                         <a class="nav-link" href="{{route('register')}}">Registrati</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item col-3">
                         <a class="nav-link" href="{{route('login')}}">Accedi</a>
                     </li>
                 @else
