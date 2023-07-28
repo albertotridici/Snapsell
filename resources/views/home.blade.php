d6f494731b3896f6388d78a443d1dbe732bd51fe
leonardo lerario <leonardo_lerario@hotmail.it>
2023-07-28 12:17:14 +0200

    aggiustate viste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,25 +17,25 @@
             <div class="swiper-slide img-custom1 d-flex flex-column justify-content-center ">
               <h1 class="h1-home watch transition">SnapSell</h1>
               <h2 class="h2-home mb-5 watch transition">Trade, Click, Smile </h2>
-              <a href="{{route('announcement.create')}}" class="watch transition btn btn-primary">Inizia a vendere!</a>
+              <a href="{{route('announcement.create')}}" class="watch transition swiper-button">Inizia a vendere!</a>
             </div>
             
             <div class="swiper-slide img-custom2 d-flex flex-column justify-content-center">
               <h1 class="h1-home">SnapSell</h1>
               <h2 class="h2-home mb-5">Consulta gli ultimi articoli pubblicati in Sport!</h2>
-              <button>Vai alla categoria.</button>
+              <a class="swiper-button" href="{{route('become.revisor')}}">Vai alla categoria.</a >
             </div>
             
             <div class="swiper-slide img-custom3 d-flex flex-column justify-content-center">
               <h1 class="h1-home">SnapSell</h1>
               <h2 class="h2-home-mus mb-5">Dai sfogo all'artista che è in te!</h2>
-              <button>Vai alla categoria.</button>
+              <a class="swiper-button" href="{{route('become.revisor')}}">Vai alla categoria.</a >
             </div>
             
             <div class="swiper-slide img-custom4 d-flex flex-column justify-content-center">
               <h1 class="h1-home">SnapSell</h1>
               <h2 class="h2-home mb-5">Entra a far parte del team!</h2>
-              <a class="btn btn-primary" href="{{route('become.revisor')}}">Invia candidatura</a >
+              <a class="swiper-button" href="{{route('become.revisor')}}">Invia candidatura</a >
             </div>
           </div>
           <div class="swiper-button-next"></div>
@@ -46,18 +46,18 @@
   </div>
 
   {{-- Start ultimi sei annunci ----------------------------------------------------------------------------------------- --}}
-  <div class="container">
+  <div class="container bg-image">
     <div class="row">
       <div class="col-12 text-center">
-        <h2 class="display-5">Articoli in tendenza</h2>
+        <h2 class="display-5 sottotitoli">Gli ultimi articoli pubblicati dai nostri utenti</h2>
       </div>
     </div>
   </div>
   <div class="container py-5">
     <div class="row">
       @foreach ($announcements as $announcement)
-      <div class="col-4 p-3 watch">
-        <div class="container-card">
+      <div class="col-12 col-lg-4 p-3">
+        <div class="container-card watch transition-card">
           <div class="img">
             <img src="https://digitshack.com/codepen/mentor13/image-product-desktop.jpg" alt="foto card">
           </div>
