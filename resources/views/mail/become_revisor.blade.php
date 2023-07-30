2f35348c745287c49ce3ca57072c80811a0d2c88
leonardo lerario <leonardo_lerario@hotmail.it>
2023-07-30 19:02:52 +0200

    aggiustata e-mail

--- resources/views/mail/become_revisor.blade.php
+++ resources/views/mail/become_revisor.blade.php
@@ -6,14 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SNAP SELL</title>
 </head>
-<body>
+<body style="background-color: rgb(244,244,244)">
     <div>
-        <h1>Un utente ha richiesto di lavorare con noi!</h1>
-        <h2>Ecco i suoi dati: </h2>
-        <p>Nome: {{$user->name}}</p>
-        <p>Email: {{$user->email}}</p>
+        <h1 style="font-size: 4vw;
+        color:rgb(0, 175, 185)" >Un utente ha richiesto di lavorare con noi!</h1>
+        <h2 style="font-size: 3vw;
+        color:rgb(0, 175, 185);">Ecco i suoi dati: </h2>
+        <h3 style="font-size: 2vw">Nome: {{$user->name}}</h3>
+        <h4 style="font-size: 2vw" >Email: {{$user->email}}</h4>
         <p>se vuoi renderlo revisore clicca qui:</p>
-        <a href="{{route('make.revisor',compact('user'))}}">Rendi revisore</a>
+        <a style="    background-color:rgb(0, 175, 185) ;
+        padding: 14px 0;
+        border-radius: 12px;
+        color: white;
+        font-size: 16px;
+        font-weight: 700;
+        cursor: pointer;
+        width: 200px;
+        display: block;
+        text-align: center;" href="{{route('make.revisor',compact('user'))}}">Rendi revisore</a>
 
     </div>
 </body>
