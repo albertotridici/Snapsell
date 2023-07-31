efe98b6b131275f0345d0b4128c816c7b3dc61c9
morris vattiata <vattiata97@gmail.com>
2023-07-31 12:37:24 +0200

    morris

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,17 +4,10 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-<<<<<<< HEAD
-        <div class="row collapse navbar-collapse justify-conten-space-around" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
-                <li class="nav-item col-3">
-                    <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
-=======
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                 <li class="nav-item color-custom">
                     <a class="color-custom nav-link active px-4" aria-current="page" href="{{route('home')}}">Home</a>
->>>>>>> 23dcb8050d128135e37a18107f87cb1aa1f73112
                 </li>
                 <li class="nav-item">
                     <a class="nav-link active color-custom px-4" aria-current="page" href="{{route('announcement.index')}}">Tutti gli Annunci</a>
@@ -30,43 +23,7 @@
                         @endforeach
                     </ul>
                 </li>
-<<<<<<< HEAD
-                    <li class="nav-item dropdown col-3">
-                        <a class="nav-link dropdown-toggle" href="#" id="categoriesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                           Categorie
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="categoriesDropdown">
-                            @foreach ($categories as $category)
-                                <li><a class="dropdown-item" href="{{route('category.show',compact('category'))}}">{{$category->name}}</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                            @endforeach
-                        </ul>
-                    </li>
-                @guest
-                    <li class="nav-item col-3">
-                        <a class="nav-link" href="{{route('register')}}">Registrati</a>
-                    </li>
-                    <li class="nav-item col-3">
-                        <a class="nav-link" href="{{route('login')}}">Accedi</a>
-                    </li>
-                @else
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Benvenuto {{Auth::user()->name}}
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="{{route('announcement.create')}}">Crea Annuncio</a></li>
-                            <li><hr class="dropdown-divider"></li>
 
-                            <li><a class="dropdown-item" role="button"
-                            onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">Log Out</a></li>
-                            <form method="POST" action="{{route('logout')}}" id="form-logout" class="d-none">@csrf</form>
-                        </ul>
-                    </li>
-                @endguest
-                
-=======
->>>>>>> 23dcb8050d128135e37a18107f87cb1aa1f73112
             </ul>
             @guest
                 <div class="nav-item px-4">
