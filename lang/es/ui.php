7655cdcd70e51d1c7de55ba95fa3a63f345e85ff
Luigi Santillan <luigisantillan2@gmail.com>
2023-08-03 09:37:50 +0200

    pull

--- lang/es/ui.php
+++ lang/es/ui.php
@@ -1,21 +1,21 @@
 <?php
-return['allAnnouncement'=>'Eccos los nostros annuncios!',
+return['allAnnouncement'=>'Aquí estan nuestros anuncios!',
        'home'=>'¡Compre ahora y ahorre!',
        'home1'=>'Crear nuevo anuncio',
        'home2'=>'Da vida a tus ofertas',
        'home3'=>'¡Publica tu anuncio ahora!',
        'navbar1'=>'Todos los anuncios',
-       'navbar2'=>'categorías',
-       'navbar3'=>'registrarse',
-       'navbar4'=>'iniciar sesión',
+       'navbar2'=>'Categorías',
+       'navbar3'=>'Registrarse',
+       'navbar4'=>'Iniciar sesión',
        'footer1'=>'Idiomas y frameworks utilizados',
-       'footer2'=>'acerca de nosotros',
+       'footer2'=>'Acerca de nosotros',
        'footer3'=>'Trabaja con nosotros',
        'card'=>'Saber más',
-       'craete'=>'crear anuncio',
+       'craete'=>'Crear anuncio',
        'success'=>'Completado con éxito',
        'danger'=>'Operación fallida',
-       'h1-show'=>'anuncio',
+       'h1-show'=>'Anuncio',
 
 ];
 
