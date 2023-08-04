00f08e9ed9752dd5f485c49fcde9bb752f8abde5
Graziano-Calella <graziano.calella@gmail.com>
2023-08-04 11:51:32 +0200

    grazyan

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -19,29 +19,24 @@
     @endif
 
     
-    <div class="toast-container position-fixed bottom-0 start-0 p-3" data-bs-delay="10000">
-        <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="toast-header">
-            {{-- <img src="..." class="rounded me-2" alt="..."> --}}
-            <strong class="me-auto">Bootstrap</strong>
-            <small>11 mins ago</small>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    <div class="toast-container" data-bs-delay="10000">
+        <div id="liveToast" class="toast-custom" role="alert" aria-live="assertive" aria-atomic="true">
+            <div>
+                @if ($ultimo)
+                    <form action="{{route('revisor.null_announcement', ['announcement' => $ultimo])}}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <button type="submit" class="btn btn-danger"><i class="fa-solid fa-share fa-rotate-180"></i></button>
+                    </form>
+                @endif
             </div>
-            <div class="toast-body">
-            Hello, world! This is a toast message.
+            <div>
+                <h6>Annulla</h6>
+                <p>Vuoi annullare l'ultima azione?</p>
             </div>
-            <div class="mt-2 pt-2 border-top">
-                <a href="{{route('home')}}" class="btn btn-primary btn-sm">Take action</a>
-                <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="toast">Close</button>
+            <div>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
-            @if ($ultimo)
-                <form action="{{route('revisor.null_announcement', ['announcement' => $ultimo])}}" method="POST">
-                    @csrf
-                    @method('PATCH')
-                    <button type="submit" class="btn btn-revisor-reject" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Rifiuta" data-bs-custom-class="tooltip-revisor-reject"><i class="fa-solid fa-xmark"></i></button>
-                </form>
-            @endif
         </div>
     </div>
-    
 </x-layout>
