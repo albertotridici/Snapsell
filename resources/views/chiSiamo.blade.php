6e17943a6c439d0c64c94419e6e82a7add5f62fb
leonardo lerario <leonardo_lerario@hotmail.it>
2023-08-07 14:38:56 +0200

    sistemata chi siamo

--- resources/views/chiSiamo.blade.php
+++ resources/views/chiSiamo.blade.php
@@ -1,16 +1,8 @@
 <x-layout>
     
-    {{-- <section class="container my-5 mt-5 pt-5 pb-5 bg-image">
-        <div class="row">
-            <div class="col-12 text-center">
-                <h1 class="h1-viste">Chi siamo</h1>
-            </div>
-        </div>
-    </section> --}}
-    
-    <section class="container-fluid min-vh-100 bg-chiSiamo">
-        <div class="row vh-100">
-            <div class="col-12 col-md-6 d-flex justify-content-center align-items-center">
+    <section class="container-fluid vh-100 bg-chiSiamo">
+        <div class="row h-100">
+            <div class="col-12 col-md-6 d-flex justify-content-center align-items-center h-custom ">
                 <div class="circle">
                     <div class="opener">
                         <i class="fa-solid fa-plus fa-3x"></i>
@@ -18,25 +10,23 @@
                 </div>
     
             </div>
-            <div class="col-12 col-md-6 d-flex justify-content-center align-items-center">
-                <div id="div-sovrapposto" class="position-absolute">
-                    <div class="container-header">
+            <div class="col-12 col-md-6 d-flex justify-content-center align-items-md-center h-custom ">
+                <div id="div-sovrapposto">
                         <div class="content-header watch transition">
                           <h2 class="h2-chiSiamo">Clicca qui per scoprire di più</h2>
                           <i class="fa-solid fa-arrow-left fa-5x" style="color: #00afb9;"></i>
                         </div>
-                      </div>
                 </div>
                 <div class="programmers-card d-none" id="transparent">
                     <div class="inner">
-                        <div class="inner-face position-relative"></div>
+                        <div class="inner-face"></div>
                         <div class="inner-back container-fluid p-0">
-                            <div class="row h-100 ">
+                            <div class="row">
                                 <div class="col-12 d-flex flex-column justify-content-around align-items-center">
-                                    <img src="/img/logo4.png" class="custom" alt="">
+                                    <img src="/img/logo4.png" class="custom-backface-img" alt="">
                                     <p class="nome-card" id="programmer-name"></p>
                                     <p class="description-card" id="programmer-description"></p>
-                                    <a id="programmer-linkedin" target="blank" href=""><i class="fa-brands fa-3x fa-linkedin" style="color: #00afb9;"></i></a>
+                                    <a id="programmer-linkedin" target="blank" href=""><i class="fa-brands fa-3x fa-linkedin " style="color: #00afb9;"></i></a>
                                 </div>
                             </div>
                         </div>   
